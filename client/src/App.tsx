34fe14da859839f914008ba260127888d63ac492
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2025-06-17 12:28:21 +0600

    a

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import CityDetail from "./components/CityDetail";
@@ -6,6 +7,26 @@ import Form from "./components/Form";
 import PostalSearch from "./components/PostalSeach";
 
 function App() {
+  useEffect(() => {
+    const sendHeight = () => {
+      const height = document.documentElement.scrollHeight;
+      window.parent.postMessage({ type: "setHeight", height }, "*");
+    };
+
+    // Send on load and resize
+    window.addEventListener("load", sendHeight);
+    window.addEventListener("resize", sendHeight);
+
+    // Also send again after 1 second to catch dynamic content
+    const timeout = setTimeout(sendHeight, 1000);
+
+    return () => {
+      window.removeEventListener("load", sendHeight);
+      window.removeEventListener("resize", sendHeight);
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <Router>
       <div>
