67e99fc1909ceecd78a28f8b4a898cc377ad1ed9
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2025-06-17 10:42:07 +0600

    add design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import CityDetail from "./components/CityDetail";
@@ -6,6 +7,18 @@ import Form from "./components/Form";
 import PostalSearch from "./components/PostalSeach";
 
 function App() {
+  useEffect(() => {
+    const updateHeight = () => {
+      const height = document.documentElement.scrollHeight;
+      window.parent.postMessage({ frameHeight: height }, "*");
+    };
+
+    updateHeight();
+    window.addEventListener("resize", updateHeight);
+
+    return () => window.removeEventListener("resize", updateHeight);
+  }, []);
+
   return (
     <Router>
       <div>
